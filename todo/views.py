295dcd66403a1ad66eb91008cd42a3af460b0e7c
Pavan Kumar <pavan082005@gmail.com>
2024-07-09 01:02:27 +0530

    form capturing and displaying the text

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import Todoform
+from django.views.decorators.http import require_POST
 # Create your views here.
 def index(request):
     todo_items = Todo.objects.order_by('id')
-    context = {'todo_items' : todo_items}
+    form = Todoform()
+    context = {'todo_items' : todo_items, 'form' : form}
     return render(request, 'todo/index.html', context)
+
+@require_POST
+def addTodo(request):
+    form = Todoform(request.POST)
+    if form.is_valid():
+        new_todo = Todo(text=request.POST['text'])
+        new_todo.save()
+    return redirect('index')
